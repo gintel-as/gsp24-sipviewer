5065ca585279068d6828b58dce489f3612df5489
Samund Ertesvåg <samund.ertesvag@gintel.com>
2024-06-11 09:14:30 +0200

    reading log

--- extractor.py
+++ extractor.py
@@ -49,106 +49,54 @@ class Extractor:
                     current_entry = [line.strip()]
                 else:
                     current_entry.append(line.strip())
-        
+            elif not timestamp_pattern.match(line):
+                current_entry.append(line.strip())
+            
         if current_entry:
             entries.append(' '.join(current_entry).strip())
 
         return entries
+    
+    def read_sip_3(self):
+        with open(self.inputFile, 'r') as file:
+            lines = file.readlines()
+        
+        timestamp_pattern = re.compile(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}')
+        
+        reading = False
+        entries = []
+        current_entry = []
 
-
-
+        for line in lines:
+            if timestamp_pattern.match(line):
+                if 'SipLogMgr' in line: #ToDo, fix for to følgende siplogmgr
+                    if reading:
+                        entries.append(current_entry)
+                        current_entry = []
+                    reading = True
+                elif reading:
+                    reading = False
+                    entries.append(current_entry)
+                    current_entry = []
+            if reading:
+                current_entry.append(line)
+        return entries
 
 
 
 if __name__ == "__main__":
     logPath = "./logs"
 
-    extractor = Extractor( logPath + "/" + "1.adapter.windows.log", "output.log")
+    extractor = Extractor( logPath + "/" + "1.adapter.windows copy.log", "output.log")
     # extractor.readLog()
     # extractor.format()
 
-    entries = extractor.read_sip_log()
-    for entry in entries:
+    entries = extractor.read_sip_3()
+    print(entries)
+    print('----------------')
+    print(entries[0])
+    print('----------------')   
+    for entry in entries[0]:
         print(entry)
-
-
-    
-
-
-
-
-
-#     def readLog(self):      # Read the log and filter out SIP packets
-#         with open(self.logFile, 'r') as inputFile, open(self.temp, 'w') as outputFile:
-#             for line in inputFile:
-#                 if 'message with id' in line:
-#                     outputFile.writelines(line)
-
-
-#     def identifyUserAgents(self, currentID):    # Identifies user agent of the specific message
-#         with open(self.temp, 'r') as inputFile:
-#             for line in inputFile:
-#                 currentUserAgent = line.split()[10]
-#                 if currentID in line and currentUserAgent in line:
-#                     legX = ''.join(re.split(r"\<.*$", currentUserAgent)[ : -1])     # Isolates "LegX" from the specific message in the log
-#                     if legX not in self.UserAgentArr:
-#                         self.UserAgentArr.append(legX)
-
-
-#     def splitTraceAndFormat(self):      # Identifies traces in the log
-#         with open(self.temp, 'r') as inputFile:
-#             for line in inputFile:
-#                 currentID = line.split()[4]
-#                 if currentID not in self.logIDArr:
-#                     self.logIDArr.append(currentID)
-
-#         for ID in self.logIDArr:
-#             self.UserAgentArr.clear()
-#             path = self.logDirPath + ID
-#             isExist = os.path.exists(path)
-#             file = self.logDirPath + ID + '.log'
-#             replacements = ["<LF><CR>", '<CR>']
-#             if not isExist:
-#                 os.makedirs(path)
-            
-#             with open(self.temp, "r") as inputFile, open(file, 'w') as outputFile:      # Separates the traces into individual logs
-#                 for line in inputFile:
-#                     if line.split()[4] == ID:
-#                         outputFile.writelines(line)
-#                 self.identifyUserAgents(ID)
-
-#             for UA in self.UserAgentArr:    # Formats packets by User Agents
-#                 legFile = self.logDirPath + ID + "_" + UA + '.log'
-#                 logTrace = self.logDirPath + ID + '.log'
-#                 with open(logTrace, "r") as inputFile, open(legFile, 'w') as outputFile:
-#                     for line in inputFile:
-#                         if UA in line:
-#                             outputFile.writelines(line)
-#                 with open(legFile, 'r') as inputFile:
-#                     content = inputFile.read()
-#                     for item in replacements:
-#                         content = content.replace(item, '\n' + '\t')
-#                 with open(legFile, 'w') as outputFile:
-#                     outputFile.write(content)
-#                 shutil.move(legFile, self.logDirPath + ID + '/' + ID + "_" + UA + '.log')
-
-#             with open(file, 'r') as inputFile:  # Formats the packets by trace
-#                 content = inputFile.read()
-#                 currentID = line.split()[4]
-#                 for item in replacements:
-#                     if currentID in line:
-#                         content = content.replace(item, '\n' + '\t')
-#             with open(file, 'w') as inputFile:
-#                 inputFile.write(content)
-#             shutil.move(file, self.logDirPath + ID + '/' + ID + '.log')
-#         os.remove(self.temp)
-
-
-#     def getLogIDarr(self):
-#         return self.logIDArr
-
-
-# if __name__ == "__main__":
-#     readAndFilterLog = logFilter('../logFiles/adapter.log')
-#     readAndFilterLog.readLog()
-#     readAndFilterLog.splitTraceAndFormat()
+    for entry in entries[1]:
+        print(entry)
