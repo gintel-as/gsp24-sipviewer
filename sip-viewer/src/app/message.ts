dbf9a6419a614ec3d147f760819df1e86a54fd57
Samund <s.ertesvag@hotmail.no>
2024-06-17 16:01:45 +0200

    Finished Interfaces

---
+++ sip-viewer/src/app/message.ts
@@ -0,0 +1,9 @@
+import { StartLine } from "./start-line";
+import { SipHeader } from "./sip-header";
+import { SipBody } from "./sip-body";
+
+export interface Message {
+    startLine: StartLine;
+    sipHeader: SipHeader;
+    body: SipBody;
+}
