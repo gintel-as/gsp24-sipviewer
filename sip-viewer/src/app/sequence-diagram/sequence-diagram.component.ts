fafe6c772e5aeb020775152dcab80f51ef1a756a
Samund <s.ertesvag@hotmail.no>
2024-06-18 12:51:14 +0200

    tmp

---
+++ sip-viewer/src/app/sequence-diagram/sequence-diagram.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sequence-diagram',
+  standalone: true,
+  imports: [],
+  templateUrl: './sequence-diagram.component.html',
+  styleUrl: './sequence-diagram.component.css'
+})
+export class SequenceDiagramComponent {
+    
+}
