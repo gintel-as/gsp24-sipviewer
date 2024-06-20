3ba082a155d429dd0e924d916c7afd243090595b
Samund <s.ertesvag@hotmail.no>
2024-06-20 15:41:08 +0200

    41: Import JSON data as diagramMessages and participants + adding selection styling on arrows

---
+++ sip-viewer/src/app/sequence-diagram/utils.ts
@@ -0,0 +1,58 @@
+import { Message } from '../message';
+import { DiagramMessage } from '../diagram-message';
+import { Observable, forkJoin, map } from 'rxjs';
+import d3, { index } from 'd3';
+import { StartLine } from '../start-line';
+
+export default class Utils {
+  static importDataFromDataServiceMap(messages: Observable<Message[]>) {
+    return messages.pipe(
+      map((msgList) =>
+        msgList.map((msg, i) => {
+          return {
+            startLine: msg.startLine,
+            index: i,
+          };
+        })
+      )
+    );
+  }
+
+  static importParseParticipants(messages: Observable<Message[]>) {
+    return messages.pipe(
+      map((msgList) =>
+        msgList.map((msg) => msg.startLine.party).filter(this.onlyUnique)
+      )
+    );
+  }
+
+  static importCombined(messages: Observable<Message[]>) {
+    return forkJoin({
+      diagramMessages: this.importDataFromDataServiceMap(messages),
+      participants: this.importParseParticipants(messages),
+    });
+  }
+
+  static onlyUnique(value: any, index: any, array: string | any[]) {
+    return array.indexOf(value) === index;
+  }
+
+  static identifyMessageRecieverAndSender(
+    startLine: StartLine,
+    defaultParty: string
+  ) {
+    if (startLine.direction == 'to') {
+      return { from: defaultParty, to: startLine.party };
+    } else {
+      return { from: startLine.party, to: defaultParty };
+    }
+  }
+
+  //Function to select color based on session string, might need to be moved elsewhere for dynamic logic
+  static selectArrowColor(session: string) {
+    if (session == '304286493') {
+      return 'red';
+    }
+    return 'blue';
+  }
+}
