89d09602d2164352f29998d3e10d3bbcaee11477
Alexander Moltu <alexandermoltu@gmail.com>
2024-06-24 14:29:05 +0200

    fetching messages by id

--- sip-viewer/src/app/message-detail/message-detail.component.ts
+++ sip-viewer/src/app/message-detail/message-detail.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { DataService } from '../data.service';
 import { NgFor } from '@angular/common';
 import { Clipboard } from '@angular/cdk/clipboard';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatIconModule } from '@angular/material/icon';
-
+import { Observable, find, first, tap } from 'rxjs';
+import { Message } from '../message';
+import { pack } from 'd3';
 
 @Component({
   selector: 'app-message-detail',
   standalone: true,
-  imports: [CommonModule, NgFor, MatToolbarModule, MatTooltipModule, MatIconModule],
+  imports: [
+    CommonModule,
+    NgFor,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatIconModule,
+  ],
   templateUrl: './message-detail.component.html',
-  styleUrl: './message-detail.component.css'
+  styleUrl: './message-detail.component.css',
 })
 export class MessageDetailComponent {
   log: any[] = [];
@@ -21,96 +29,107 @@ export class MessageDetailComponent {
   textToCopy: any[] = [];
   packetIndex: number = 0;
 
-  constructor(
-    private dataService: DataService,
-    private clipboard: Clipboard
-  ) { }
+  currentMessage: Message | undefined;
 
-  ngOnInit(): void {
+  currentMessageId: string = '';
+  messageIdList: string[] = []; // Remove later
+
+  constructor(private dataService: DataService, private clipboard: Clipboard) {}
+
+  ngOnInit() {
     this.fetchMessages();
   }
 
-  changePacket(direction: 'next'|'previous') {
+  ngAfterViewInit() {}
+
+  changePacket(direction: 'next' | 'previous') {
     if (direction == 'next') {
-      if ((this.packetIndex + 1) > this.log.length) {
-      } else {
+      if (this.packetIndex + 1 < this.messageIdList.length) {
         this.packetIndex++;
-        this.printPacketDetail();
+        const id = this.messageIdList[this.packetIndex];
+        this.printPacketDetail(id);
       }
     } else if (direction == 'previous') {
-      if ((this.packetIndex - 1) < 0) {
-      } else {
+      if (this.packetIndex - 1 >= 0) {
         this.packetIndex--;
-        this.printPacketDetail();
+        const id = this.messageIdList[this.packetIndex];
+        this.printPacketDetail(id);
       }
     }
   }
 
-  copyText() {
-    let output = this.textToCopy.join('');
-    this.clipboard.copy(output);
+  findInJsonByMessageId(id: string) {
+    // return this.dataService.getMessageByID(id);
+    return this.dataService.getMessageByID(id).pipe(
+      tap((message: Message | undefined) => {
+        this.currentMessage = message;
+      })
+    );
   }
 
-  findInJson(inputArr: any[], index: number, str: 'sipHeader'|'body'): string[] {
-    let output: string[] = [];
-    let headers;
-    
-    if (str == 'sipHeader') {
-      headers = inputArr[index].sipHeader;
-
-      for (const key in headers) {
-        if (headers.hasOwnProperty(key)) {
-          headers[key].forEach((value: string) => {
-            let str: string = '';
-            if (key == 'Header') {
-              str = value;
-            } else {
-              str = key + ': ' + value;
-            }
-            this.textToCopy.push(str + '\n')
-            output.push(str);
-          });
-        }
-      }
-    } else if (str == 'body') {
-      headers = inputArr[index].body;
-
-      for (const key in headers) {
-        if (headers.hasOwnProperty(key)) {
-          headers[key].forEach((value: string) => {
-            const str = value;
-            this.textToCopy.push(str + '\n')
-            output.push(str);
-          });
-        }
-      }
-    }
-    return output;
-  }
-
-  printPacketDetail(): void {
+  printPacketDetail(id: string): void {
     this.textToCopy = [];
-    this.resultList = [
-      ...this.findInJson(this.log, this.packetIndex, 'sipHeader'),
-      ...this.findInJson(this.log, this.packetIndex, 'body')
-    ];
+    this.resultList = [];
+
+    this.findInJsonByMessageId(id).subscribe(
+      (message: Message | undefined) => {
+        let output: string[] = [];
+        let sipHeaderArr = message?.sipHeader;
+        let bodyArr = message?.body;
+
+        // stringifies sipHeader for printing
+        for (const key in sipHeaderArr) {
+          if (sipHeaderArr.hasOwnProperty(key)) {
+            sipHeaderArr[key].forEach((value: string) => {
+              let str: string = '';
+              if (key == 'Header') {
+                str = value;
+              } else {
+                str = key + ': ' + value;
+              }
+              this.textToCopy.push(str + '\n');
+              output.push(str);
+            });
+          }
+        }
+
+        // stringifies body for printing
+        if (bodyArr && bodyArr.content) {
+          bodyArr.content.forEach((value) => {
+            const str = value;
+            this.textToCopy.push(str + '\n');
+            output.push(str);
+          });
+        }
+
+        this.resultList = output;
+      },
+      (error) => {
+        console.error('Error printing message with id = ' + id, error);
+      }
+    );
   }
-  
+
   fetchMessages(): void {
     this.dataService.getMessages().subscribe(
       (messages: any[]) => {
         this.log = messages;
-        this.printPacketDetail();
+        this.messageIdList = messages.map((item) => item.startLine.messageID);
+        this.printFirstMessage();
       },
-      error => {
+      (error) => {
         console.error('Error fetching messages', error);
       }
     );
   }
 
+  printFirstMessage(): void {
+    this.currentMessageId = this.messageIdList[0];
+    this.printPacketDetail(this.currentMessageId);
+  }
 
-
+  copyText(): void {
+    let output = this.textToCopy.join('');
+    this.clipboard.copy(output);
+  }
 }
-
-
-
