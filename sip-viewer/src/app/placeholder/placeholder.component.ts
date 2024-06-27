d02a4e20a22d006f511c8242f67e526dd476b869
Alexander Moltu <alexandermoltu@gmail.com>
2024-06-27 14:46:12 +0200

    83: added placeholder component

---
+++ sip-viewer/src/app/placeholder/placeholder.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-placeholder',
+  standalone: true,
+  imports: [],
+  templateUrl: './placeholder.component.html',
+  styleUrl: './placeholder.component.css',
+})
+export class PlaceholderComponent {}
