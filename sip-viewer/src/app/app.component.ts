243d61c35e4122aa470dd638f46e72c496b000ce
Alexander Moltu <alexandermoltu@gmail.com>
2024-07-01 14:49:03 +0200

    105: added svg logo

--- sip-viewer/src/app/app.component.ts
+++ sip-viewer/src/app/app.component.ts
@@ -11,9 +11,7 @@ import { FileUploadComponent } from './file-upload/file-upload.component';
 // Import material design
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { DomSanitizer } from '@angular/platform-browser';
 import { MatIconModule } from '@angular/material/icon';
-import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -34,17 +32,6 @@ import { MatIconRegistry } from '@angular/material/icon';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  // Supposed to render svg logo, but does not work
-  constructor(
-    private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer
-  ) {
-    this.matIconRegistry.addSvgIcon(
-      'gintel',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('gintel-logo-main.svg')
-    );
-  }
-
   resizing = false;
   horizontalResize = false;
   verticalResize = false;
