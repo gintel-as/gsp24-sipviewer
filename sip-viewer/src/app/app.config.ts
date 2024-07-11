d6d3dad7021f58428b39662ad2ef88b0703490fa
Samund <s.ertesvag@hotmail.no>
2024-07-11 09:04:12 +0200

    157: Removed unintended changes

--- sip-viewer/src/app/app.config.ts
+++ sip-viewer/src/app/app.config.ts
@@ -1,16 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
 import routes from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(
-      routes,
-      withInMemoryScrolling({
-        scrollPositionRestoration: 'enabled',
-      })
-    ),
+    provideRouter(routes),
   ],
 };
