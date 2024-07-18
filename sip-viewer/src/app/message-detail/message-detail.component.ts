6f800715fac3d7034e1126a7c1e56e9d72286ba8
Helle Svåsand <helsva@live.no>
2024-07-18 09:04:04 +0200

    215: changed from 3 to 1 spaces between header and body

--- sip-viewer/src/app/message-detail/message-detail.component.ts
+++ sip-viewer/src/app/message-detail/message-detail.component.ts
@@ -130,10 +130,8 @@ export class MessageDetailComponent {
         // Stringifies body for printing
         if (bodyArr && bodyArr.content) {
           //Add gap between header and body
-          for (let i = 0; i < 3; i++) {
-            detailsOutput.push('');
-            this.textToCopy.push('' + '\n');
-          }
+          detailsOutput.push('');
+          this.textToCopy.push('' + '\n');
           //Add body
           bodyArr.content.forEach((value) => {
             const str = value;
